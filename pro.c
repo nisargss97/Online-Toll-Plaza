55adfb508b4bbd02ecfa56cc51d99c579ef809ab
nisargss97 <nisargss97@gmail.com>
2017-01-18 15:28:04 +0530

    Initial Project
    Working User Login/Registration function.

---
+++ pro.c
@@ -0,0 +1,290 @@
+#include <stdio.h>
+//#include <conio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>   
+
+#define ff fflush(stdin);
+typedef struct{
+	char id[21];
+	char pwd[21];
+
+} user;
+
+int user_func();
+int user_reg();
+void user_profile();
+int admin_func();
+
+int main()
+{
+	char ch,c;
+	int choice;
+	int flag=1;
+ 
+	do 
+	{
+		system("clear");
+		printf("Welcome to Toll Plaza\n\n");
+		printf("Please select from options below : \n\n");
+		printf("1. User \n");
+		printf("2. Administrator \n");
+		printf("3. Exit \n\n");	
+		scanf("%d", &choice);ff;
+
+ 		switch(choice)
+		{
+			case 1:	
+					user_func();
+					break;
+			case 2:
+					admin_func();
+					break;
+			case 3: 
+					ff;getchar();system("clear");
+					printf("\nDo you really want to exit?(Y/N) : \n\n");	
+					ff;scanf(" %c",&c);
+					if(c=='y'||c=='Y')
+						exit(0);
+					break;
+			default:
+					ff;getchar();
+					system("clear");
+					printf("\nWrong choice. \n\nPress Enter to continue.\n");ff;getchar();
+					system("clear");
+					break;
+ 
+		}
+ 
+	} while(1);
+
+	return 0;		
+}
+ 
+ 
+int user_func()
+{
+	
+
+	FILE* f;
+	char ch,c;
+	char id[21]="";
+	char s[12];
+	char pwd[21];	
+	user u;
+	int i=0;
+	int flag;
+	int flag1;
+
+	f=fopen("user_data.txt","ab+");
+	if(f==NULL)
+	{
+		system("clear");
+		printf("\n404 File not found. \n\nPlease try again later. \n");
+		fclose(f);
+		return -1;
+	}
+	
+	c='a';
+	
+	do
+	{
+		ch=c;
+		ff; getchar();	system("clear");
+		printf("\nAre you a registered user? \n");
+		printf("\n[Enter M to go back to main menu.] \n\nSelect your choice (Y/N/M)\n\n");
+		scanf(" %c",&ch);
+		ff getchar();
+		switch(ch)
+		{
+			case 'Y':
+			case 'y':
+						printf("\nEnter your login details: \n\n");
+						printf("Car no : ");
+						scanf("%s",id);
+						ff; getchar();
+						char *A;					
+						fseek(f,0,SEEK_SET);
+						flag=0;
+
+						while(!feof(f))
+						{
+							fread(&u,sizeof(u),1,f);
+						
+							if(strcmp(u.id,id)==0)
+							{
+								flag=1;
+								do{
+
+									A=getpass("\nPassword : ");
+									strcpy(pwd,A);
+									
+									if(strcmp(pwd,u.pwd)==0)
+									{
+										break;
+									}
+						
+//									if(i==2){i++;break;}
+						
+									printf("\n\n%d attempts left",2-i );
+									i++;
+
+								} while(i<3);
+
+								if(i==3)
+								{
+									printf("\n\n Password attempt unsuccesful thrice. \n\n");
+									fclose(f);	
+									return -1;
+								}
+								
+
+								system("clear");
+
+								printf("\n\nSuccessfully logged in.  \nPress Enter to continue.\n");
+								ff; getchar(); ff;
+
+								user_profile();			
+								flag=1;
+								break;
+							}
+
+						}
+
+						if(!flag)
+						{
+							printf("\nUser not found. \n");
+						}
+
+
+						break;
+			
+			case 'N':
+			case 'n':
+						system("clear");
+						printf("\nYou have to register before going forward.");	
+						printf("\n\nPress enter to continue. \n\n");
+						ff;getchar();system("clear");
+
+						flag1=user_reg();
+
+
+						break;
+
+			case 'M':
+			case 'm':
+						system("clear");
+						printf("\nDo you really want to return to main menu?(Y/N) : \n\n");	
+						ff;scanf(" %c",&c);
+						fclose(f);
+						if(c=='y'||c=='Y')
+							return 0;	
+						break;
+
+			default:
+						system("clear");
+						printf("\nWrong choice. \n");
+						printf("\nPress Enter to continue. \n\n");			
+						break;
+						
+
+		}
+
+	}while(1);
+	
+	fclose(f);
+	return 1;
+}
+
+int user_reg()
+{
+
+	char ch;
+	char *A,*B;
+	int flag;
+	user u,v;
+	FILE* f=fopen("user_data.txt","ab+");
+
+	printf("\nYou are being redirected to registration page. ");
+	printf("\n\nPress q followed by Enter to go back.\n");
+	printf("\nPress Enter to continue.\n\n");
+	ff;scanf("%c",&ch);
+
+	if(ch=='q'||ch=='Q')
+	{
+		fclose(f);
+		return -1;
+	}
+	ff;system("clear");
+
+	printf("\nREGISTRATION PAGE.\n\n");
+	printf("Enter the following details : \n\n");
+	
+	do{
+		
+		flag=1;
+		printf("Car no : ");
+		ff
+		scanf("%s",u.id);					
+	
+		fseek(f,0,SEEK_SET);
+	
+		while(!feof(f))
+		{				
+			fread(&v,sizeof(u),1,f);
+			if(strcmp(u.id,v.id)==0)
+			{
+				flag=0;
+				break;
+			}
+		}
+		if(!flag)
+		{
+			ff;system("clear");
+			printf("\nUser Name already exists. \n\nReEnter ");
+		}
+		else
+			break;
+	}while(1);
+
+	while(1)
+	{
+		ff
+		A=getpass("\nPassword : ");
+		strcpy(u.pwd,A);ff;
+		B=getpass("\nConfirm Password : ");ff;
+//		printf("\n%s",u.pwd);
+//		printf("\n%s",B);
+
+		if(strcmp(u.pwd,B)==0)
+		{
+			break;
+		}
+		else
+		{
+			printf("\n\nPasswords do not match. ReEnter :\n");
+		}
+	}
+
+	strcpy(u.pwd,A);
+
+	fwrite(&u,sizeof(u),1,f);
+
+	fclose(f);	
+	printf("\nUser registered successfully .");
+	ff getchar();system("clear");
+	return 1;
+
+}
+
+void user_profile()
+{
+
+	return;
+}
+
+int admin_func()
+{
+
+	return 1;
+}
